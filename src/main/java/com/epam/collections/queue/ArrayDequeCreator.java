57028a9cec81d3737a8a684f1429f9eeb8fd4cdc
InDev <InDevRus@hotmail.com>
2023-04-04 01:10:59 +0500

    Solved

--- src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
+++ src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
@@ -5,6 +5,31 @@ import java.util.PriorityQueue;
 import java.util.Queue;
 
 public class ArrayDequeCreator extends PriorityQueue<String> {
+    private void disposeCard(Queue<Integer> source, Queue<Integer> destination) {
+        destination.add(source.remove());
+    }
+
+    private void moveCards(Queue<Integer> playersCards, ArrayDeque<Integer> sharedCards) {
+        if (playersCards.isEmpty()) {
+            return;
+        }
+
+        playersCards.add(sharedCards.removeLast());
+        sharedCards.add(playersCards.remove());
+        sharedCards.add(playersCards.remove());
+    }
     public ArrayDeque<Integer> createArrayDeque(Queue<Integer> firstQueue, Queue<Integer> secondQueue) {
+        var sharedCards = new ArrayDeque<Integer>();
+        disposeCard(firstQueue, sharedCards);
+        disposeCard(firstQueue, sharedCards);
+        disposeCard(secondQueue, sharedCards);
+        disposeCard(secondQueue, sharedCards);
+
+        while (!firstQueue.isEmpty() || !secondQueue.isEmpty()) {
+            moveCards(firstQueue, sharedCards);
+            moveCards(secondQueue, sharedCards);
+        }
+
+        return sharedCards;
     }
 }
--- src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
+++ src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
@@ -1,8 +1,44 @@
 package com.epam.collections.queue;
 
+import java.util.AbstractSequentialList;
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class DishOrderDeterminer {
+    private AbstractSequentialList<Integer> generateDishes(int numberOfDishes) {
+        var cycledDishesList = new LinkedList<Integer>();
+        for (var dish = 1; dish <= numberOfDishes; dish += 1) {
+            cycledDishesList.addLast(dish);
+        }
+        return cycledDishesList;
+    }
+
+    private void eatDishes(List<Integer> dishesOrder, AbstractSequentialList<Integer> cycledDishesList, int everyDishNumberToEat) {
+        var dishNumber = 0;
+        var dishesEaten = 0;
+        while (dishesEaten < everyDishNumberToEat && !cycledDishesList.isEmpty()) {
+            var linkedIterator = cycledDishesList.listIterator();
+            while (linkedIterator.hasNext()) {
+                dishNumber += 1;
+                var currentDish = linkedIterator.next();
+                if (dishNumber % everyDishNumberToEat == 0) {
+                    linkedIterator.remove();
+                    dishesOrder.add(currentDish);
+                    dishesEaten += 1;
+                }
+            }
+        }
+    }
+
     public List<Integer> determineDishOrder(int numberOfDishes, int everyDishNumberToEat) {
+        var dishesOrder = new ArrayList<Integer>();
+        var cycledDishesList = generateDishes(numberOfDishes);
+
+        while (!cycledDishesList.isEmpty()) {
+            eatDishes(dishesOrder, cycledDishesList, everyDishNumberToEat);
+        }
+
+        return dishesOrder;
     }
 }
